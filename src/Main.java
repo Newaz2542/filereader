56641957101f79a3d2952d15bc5f54439b70a93e
Vichakorn Yotboonrueang <viyotboon_new@outlook.com>
2018-02-26 18:36:54 +0700

    fix location of alice.txt

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
-/**
- * This class is for running all the code.
- * @author Vichakorn Yotboonrueang
- *
- */
+
 public class Main {
-	/**
-	 * Call TaskTimer.measureAndPrint by different of reader and run.
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
 		TaskTimer.measureAndPrint(new ReadByString());
 		TaskTimer.measureAndPrint(new ReadByBufferedReader());
